92a09848e4b5ef92562956c4351af80d69107d08
max <kingcall550102@gmail.com>
2025-03-06 21:38:26 +0800

    add test message

--- hello/tests.py
+++ hello/tests.py
@@ -9,3 +9,4 @@ class HelloViewTest(TestCase):
         response = self.client.get(reverse('hello'))  # 測試 /hello/
         self.assertEqual(response.status_code, 200)  # 確保回應 200
         self.assertJSONEqual(response.content, {"message": "Hello, Django!"})  # 確保 JSON 正確
+        print("一階段測試 OK")
