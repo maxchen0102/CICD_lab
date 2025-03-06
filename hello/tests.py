c1f682e8220e471600b79f9d552bb1eca4cd79be
max <kingcall550102@gmail.com>
2025-03-06 21:34:37 +0800

    django

---
+++ hello/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+class HelloViewTest(TestCase):
+    def test_hello_view(self):
+        response = self.client.get(reverse('hello'))  # 測試 /hello/
+        self.assertEqual(response.status_code, 200)  # 確保回應 200
+        self.assertJSONEqual(response.content, {"message": "Hello, Django!"})  # 確保 JSON 正確
